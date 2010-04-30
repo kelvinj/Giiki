54ad8f9cd67c986bca10b146faa3234cb4487f2e
Kelvin Jones <kelvinj@gmail.com>
2010-04-30 12:41:50 +0100

    tweaking defaults

--- giiki/options.php
+++ giiki/options.php
@@ -1,8 +1,8 @@
 <?php
 $giiki_options = array(
 	'repo' => 'data',
-	'name' => 'Kelvin Jones Wiki',
-	'git_bin' => '/usr/local/git/bin/git',
+	'name' => 'Wiki Name',
+	'git_bin' => '/usr/bin/git',
 	'index_page_content_in_db' => true, /* otherwise searching functionality should be removed (_header.php) */
 	'autolink_urls' => true
 );
